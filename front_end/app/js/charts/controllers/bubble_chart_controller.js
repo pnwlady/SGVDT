7702ba37c4629285984dbeee056f4165b86ea946
heidilaursen <heidimlaursen@gmail.com>
2016-06-07 16:08:09 -0700

    bubble and line controller start

--- front_end/app/js/charts/controllers/bubble_chart_controller.js
+++ front_end/app/js/charts/controllers/bubble_chart_controller.js
@@ -1,1 +1,8 @@
 const angular = require('angular');
+
+
+module.exports = function(app) {
+    app.controller('chartController', function($scope) {
+    $scope.chart = 
+
+};
---
+++ front_end/app/js/charts/controllers/line_graph_controller.js
@@ -0,0 +1,7 @@
+const angular = require('angular');
+
+module.exports = function(app) {
+    app.controller('lineController', function($scope) {
+    $scope.line =
+
+};
