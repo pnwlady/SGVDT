b214b3460afc7b516510232f78dddb65faf74ef2
heidilaursen <heidimlaursen@gmail.com>
2016-06-09 14:08:16 -0700

    router changes, but a bit broken

--- server/router/user_router.js
+++ server/router/user_router.js
@@ -1,25 +1,43 @@
 const express = require('express');
+const handleError = require('/../lib/error_handler');
 const User = require(__dirname + '/../model/user');
 const jsonParser = require('body-parser').json();
+const jwtToken = require('/../lib/jwt_auth');
 const basicHTTP = require(__dirname + '/../lib/basic_http');
+var authRouter = module.exports = exports = express.Router();
 
-var router = module.exports = exports = express.Router();
+authRouter.post('/signup', jsonParser, (req, res) => {
+  User.findOne({ 'username': req.body.username }, (req, res) => {
+    if (!user) {
+      var user = new User();
+      user.auth.basic.username = req.body.username;
+      user.username = req.body.username;
+      // user.zipcode = req.body.zipcode;
+      
+      user.save((err, data) => {
+        if (user.username === '' || req.body.password === '') {
+          handleError(err, res);
+          data.generateToken((err, token) => {
+            res.json({ token: token });
+          });
+      } else {
+        process.stdout.write('user already exists');
+        return res.status(401).json({ msg: 'user by that name exists' });
+        }
+      });
+    }
+  });
+});
 
-router.post('/signup', jsonParser, (req, res) => {
+authRouter.post('/signup', jsonParser, (req, res) => {
+  var newUser = new User(req.body);
   var password = req.body.password;
-  var zipcode = req.body.zipcode;
   req.body.password = null;
 
   if (!password) {
     return res.status(500).json({ msg: 'add a password' });
   }
 
-  if (!zipcode) {
-    return res.status(500).json({ msg: 'add your zipcode' });
-  }
-
-  var newUser = new User(req.body);
-  var zipcode = req.body.zipcode;
   newUser.generateHash(password);
   password = null;
 
@@ -38,7 +56,8 @@ router.post('/signup', jsonParser, (req, res) => {
   });
 });
 
-router.get('/signin', basicHTTP, (req, res) => {
+authRouter.get('/signin', basicHTTP, (req, res) => {
+  var token = User.generateToken();
   User.findOne({ username: req.auth.username }, (err, user) => {
     if (err) {
       return res.status(500).json({ msg: 'no authentication' });
@@ -51,11 +70,13 @@ router.get('/signin', basicHTTP, (req, res) => {
     if (!user.compareHash(req.auth.password)) {
       return res.status(500).json({ msg: 'could not authenticate' });
     }
-
-    var token = user.generateToken();
     if (err) {
       return res.status(500).json({ msg: 'could not generate token' });
     }
     res.json({ token });
   });
+});
+
+authRouter.get('/users', jwtToken, (req, res) => {
+  res.json({ username: req.user.username });
 });
