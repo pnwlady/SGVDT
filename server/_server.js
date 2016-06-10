7ad0599486523c7a3006b1f94aabc96141c2bba3
heidilaursen <heidimlaursen@gmail.com>
2016-06-10 10:40:40 -0700

    server updates

--- server/_server.js
+++ server/_server.js
@@ -5,13 +5,13 @@ const offensesRouter = require(__dirname + '/router/offenses_router');
 const userRouter = require(__dirname + '/router/user_router');
 const newsRouter = require(__dirname + '/router/news_router');
 
+app.use(express.static(__dirname + '/build'));
 app.use(function(req, res, next) {
 res.header('Access-Control-Allow-Origin', '*');
 res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, token');
 res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 next();
 });
-app.use('', express.static(__dirname + '/../front_end/app/build'));
 app.use('/api', newsRouter);
 app.use('/api', offensesRouter);
 app.use('/', userRouter);
