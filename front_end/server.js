830eb911a000d5a038e872c7ebef10d349bb7529
heidilaursen <heidimlaursen@gmail.com>
2016-06-10 10:19:40 -0700

    adding frone end static servre to backend

--- front_end/server.js
+++ front_end/server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-var port = process.env.PORT;
-
-express().use(express.static(__dirname + '/build'))
-.get('*', function(req, res) {
-  res.redirect('/#' + req.url);
-}).listen(port, () => console.log('server up on ' + port));
-
-remove baseURL '/api'
+// const express = require('express');
+// var port = process.env.PORT;
+//
+// express().use(express.static(__dirname + '/build'))
+// .get('*', function(req, res) {
+//   res.redirect('/#' + req.url);
+// }).listen(port, () => console.log('server up on ' + port));
+//
+// remove baseURL '/api'
